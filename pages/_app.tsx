b8f0fdc9072e3d19e7a589a783c17b7a2bd0b1ca
Daniel Magro <dando385@yahoo.com>
2024-02-29 22:07:35 -0500

    -m

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-import 'tailwindcss/tailwind.css';
-
-
+import '../styles/globals.css';
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Header from "../components/header";
